2ceef584b8ce9779211fa332b7fea640578b07eb
hvtanh07 <32663098+hvtanh07@users.noreply.github.com>
2021-11-24 16:45:18 +0700

    update timer

--- View/MainWindow.Designer.cs
+++ View/MainWindow.Designer.cs
@@ -32,6 +32,7 @@ namespace MESdbToERPdb
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mes2ERPMainWin));
             this.panel_Controller = new System.Windows.Forms.Panel();
+<<<<<<< Updated upstream
             this.btn_stopTimer = new System.Windows.Forms.Button();
             this.btn_startTimer = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
@@ -41,15 +42,26 @@ namespace MESdbToERPdb
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+=======
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_timeRec = new System.Windows.Forms.MaskedTextBox();
+            this.lb_progress = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.lb_logText = new System.Windows.Forms.Label();
+            this.pgb_backgroundWorkerProgressBar = new System.Windows.Forms.ProgressBar();
+>>>>>>> Stashed changes
             this.btn_stop = new System.Windows.Forms.Button();
             this.btn_start = new System.Windows.Forms.Button();
             this.pic_logo = new System.Windows.Forms.PictureBox();
             this.panel_LogText = new System.Windows.Forms.Panel();
+<<<<<<< Updated upstream
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+=======
+            this.dtgv_Uploaded = new System.Windows.Forms.DataGridView();
+>>>>>>> Stashed changes
             this.lb_progress_percentage = new System.Windows.Forms.Label();
-            this.lb_progress = new System.Windows.Forms.Label();
-            this.lb_logText = new System.Windows.Forms.Label();
             this.btn_setting = new System.Windows.Forms.Button();
+<<<<<<< Updated upstream
             this.btn_support = new System.Windows.Forms.Button();
             this.pgb_backgroundWorkerProgressBar = new System.Windows.Forms.ProgressBar();
             this.pic_title = new System.Windows.Forms.PictureBox();
@@ -57,14 +69,29 @@ namespace MESdbToERPdb
             this.timer_nextRun = new System.Windows.Forms.Timer(this.components);
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+=======
+            this.pic_title = new System.Windows.Forms.PictureBox();
+            this.bgWorker = new System.ComponentModel.BackgroundWorker();
+            this.tmrCallBgWorker = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timmer = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.richTextBoxLog1 = new MESdbToERPdb.RichTextBoxLog();
+>>>>>>> Stashed changes
             this.panel_Controller.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.txt_cycle)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pic_logo)).BeginInit();
             this.panel_LogText.SuspendLayout();
+<<<<<<< Updated upstream
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pic_title)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+=======
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv_Uploaded)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_title)).BeginInit();
+>>>>>>> Stashed changes
             this.SuspendLayout();
             // 
             // panel_Controller
@@ -161,6 +188,7 @@ namespace MESdbToERPdb
             this.textBox1.Size = new System.Drawing.Size(344, 22);
             this.textBox1.TabIndex = 6;
             // 
+<<<<<<< Updated upstream
             // btn_stop
             // 
             this.btn_stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -171,6 +199,40 @@ namespace MESdbToERPdb
             this.btn_stop.Text = "STOP";
             this.btn_stop.UseVisualStyleBackColor = true;
             this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+=======
+            this.groupBox1.Controls.Add(this.timmer);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txt_timeRec);
+            this.groupBox1.Controls.Add(this.elementHost1);
+            this.groupBox1.Controls.Add(this.lb_progress);
+            this.groupBox1.Controls.Add(this.btn_start);
+            this.groupBox1.Controls.Add(this.lb_logText);
+            this.groupBox1.Controls.Add(this.pgb_backgroundWorkerProgressBar);
+            this.groupBox1.Controls.Add(this.btn_stop);
+            this.groupBox1.Location = new System.Drawing.Point(3, 105);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(415, 541);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+>>>>>>> Stashed changes
+            // 
+            // txt_timeRec
+            // 
+            this.txt_timeRec.Location = new System.Drawing.Point(145, 149);
+            this.txt_timeRec.Mask = "0000-00-00 00:00:00.000";
+            this.txt_timeRec.Name = "txt_timeRec";
+            this.txt_timeRec.Size = new System.Drawing.Size(251, 22);
+            this.txt_timeRec.TabIndex = 8;
+            // 
+            // lb_progress
+            // 
+            this.lb_progress.AutoSize = true;
+            this.lb_progress.Font = new System.Drawing.Font("Century", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_progress.Location = new System.Drawing.Point(353, 499);
+            this.lb_progress.Name = "lb_progress";
+            this.lb_progress.Size = new System.Drawing.Size(53, 22);
+            this.lb_progress.TabIndex = 5;
+            this.lb_progress.Text = "Start";
             // 
             // btn_start
             // 
@@ -183,6 +245,37 @@ namespace MESdbToERPdb
             this.btn_start.UseVisualStyleBackColor = true;
             this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
             // 
+<<<<<<< Updated upstream
+=======
+            // lb_logText
+            // 
+            this.lb_logText.AutoSize = true;
+            this.lb_logText.Font = new System.Drawing.Font("Century", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_logText.Location = new System.Drawing.Point(6, 253);
+            this.lb_logText.Name = "lb_logText";
+            this.lb_logText.Size = new System.Drawing.Size(112, 21);
+            this.lb_logText.TabIndex = 4;
+            this.lb_logText.Text = "Log output : ";
+            // 
+            // pgb_backgroundWorkerProgressBar
+            // 
+            this.pgb_backgroundWorkerProgressBar.Location = new System.Drawing.Point(6, 499);
+            this.pgb_backgroundWorkerProgressBar.Name = "pgb_backgroundWorkerProgressBar";
+            this.pgb_backgroundWorkerProgressBar.Size = new System.Drawing.Size(341, 36);
+            this.pgb_backgroundWorkerProgressBar.TabIndex = 2;
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_stop.Location = new System.Drawing.Point(220, 35);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(176, 90);
+            this.btn_stop.TabIndex = 5;
+            this.btn_stop.Text = "STOP";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+>>>>>>> Stashed changes
             // pic_logo
             // 
             this.pic_logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
@@ -210,6 +303,7 @@ namespace MESdbToERPdb
             this.panel_LogText.Size = new System.Drawing.Size(811, 649);
             this.panel_LogText.TabIndex = 1;
             // 
+<<<<<<< Updated upstream
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToOrderColumns = true;
@@ -222,6 +316,17 @@ namespace MESdbToERPdb
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(800, 421);
             this.dataGridView1.TabIndex = 7;
+=======
+            // dtgv_Uploaded
+            // 
+            this.dtgv_Uploaded.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgv_Uploaded.Location = new System.Drawing.Point(4, 140);
+            this.dtgv_Uploaded.Name = "dtgv_Uploaded";
+            this.dtgv_Uploaded.RowHeadersWidth = 51;
+            this.dtgv_Uploaded.RowTemplate.Height = 24;
+            this.dtgv_Uploaded.Size = new System.Drawing.Size(804, 500);
+            this.dtgv_Uploaded.TabIndex = 7;
+>>>>>>> Stashed changes
             // 
             // lb_progress_percentage
             // 
@@ -232,6 +337,7 @@ namespace MESdbToERPdb
             this.lb_progress_percentage.Size = new System.Drawing.Size(0, 22);
             this.lb_progress_percentage.TabIndex = 6;
             // 
+<<<<<<< Updated upstream
             // lb_progress
             // 
             this.lb_progress.AutoSize = true;
@@ -252,6 +358,8 @@ namespace MESdbToERPdb
             this.lb_logText.TabIndex = 4;
             this.lb_logText.Text = "Log output : ";
             // 
+=======
+>>>>>>> Stashed changes
             // btn_setting
             // 
             this.btn_setting.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_setting.BackgroundImage")));
@@ -263,6 +371,7 @@ namespace MESdbToERPdb
             this.btn_setting.UseVisualStyleBackColor = true;
             this.btn_setting.Click += new System.EventHandler(this.btn_setting_Click);
             // 
+<<<<<<< Updated upstream
             // btn_support
             // 
             this.btn_support.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_support.BackgroundImage")));
@@ -281,6 +390,8 @@ namespace MESdbToERPdb
             this.pgb_backgroundWorkerProgressBar.Size = new System.Drawing.Size(804, 36);
             this.pgb_backgroundWorkerProgressBar.TabIndex = 2;
             // 
+=======
+>>>>>>> Stashed changes
             // pic_title
             // 
             this.pic_title.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pic_title.BackgroundImage")));
@@ -293,17 +404,19 @@ namespace MESdbToERPdb
             this.pic_title.TabIndex = 0;
             this.pic_title.TabStop = false;
             // 
-            // BW
+            // bgWorker
             // 
-            this.BW.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BW_DoWork);
-            this.BW.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BW_ProgressChanged);
-            this.BW.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BW_RunWorkerCompleted);
+            this.bgWorker.WorkerReportsProgress = true;
+            this.bgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BW_DoWork);
+            this.bgWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BW_ProgressChanged);
+            this.bgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BW_RunWorkerCompleted);
             // 
-            // timer_nextRun
+            // tmrCallBgWorker
             // 
-            this.timer_nextRun.Interval = 1000;
-            this.timer_nextRun.Tick += new System.EventHandler(this.timer_nextRun_Tick);
+            this.tmrCallBgWorker.Interval = 1000;
+            this.tmrCallBgWorker.Tick += new System.EventHandler(this.timer_nextRun_Tick);
             // 
+<<<<<<< Updated upstream
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.btn_start);
@@ -332,6 +445,43 @@ namespace MESdbToERPdb
             this.groupBox2.TabIndex = 18;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Automated";
+=======
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 21);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Prev record";
+            // 
+            // timmer
+            // 
+            this.timmer.AutoSize = true;
+            this.timmer.Font = new System.Drawing.Font("Century", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timmer.Location = new System.Drawing.Point(93, 203);
+            this.timmer.Name = "timmer";
+            this.timmer.Size = new System.Drawing.Size(104, 21);
+            this.timmer.TabIndex = 10;
+            this.timmer.Text = "Prev record";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "MestoERP";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.elementHost1.Location = new System.Drawing.Point(6, 277);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(403, 209);
+            this.elementHost1.TabIndex = 7;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.richTextBoxLog1;
+>>>>>>> Stashed changes
             // 
             // mes2ERPMainWin
             // 
@@ -350,12 +500,17 @@ namespace MESdbToERPdb
             ((System.ComponentModel.ISupportInitialize)(this.pic_logo)).EndInit();
             this.panel_LogText.ResumeLayout(false);
             this.panel_LogText.PerformLayout();
+<<<<<<< Updated upstream
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pic_title)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+=======
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv_Uploaded)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_title)).EndInit();
+>>>>>>> Stashed changes
             this.ResumeLayout(false);
 
         }
@@ -374,6 +529,7 @@ namespace MESdbToERPdb
         private System.Windows.Forms.Label lb_progress;
         private System.Windows.Forms.Button btn_stop;
         private System.Windows.Forms.Button btn_start;
+<<<<<<< Updated upstream
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
@@ -388,6 +544,18 @@ namespace MESdbToERPdb
         private System.Windows.Forms.Timer timer_nextRun;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox1;
+=======
+        private System.ComponentModel.BackgroundWorker bgWorker;
+        private System.Windows.Forms.Timer tmrCallBgWorker;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private System.Windows.Forms.DataGridView dtgv_Uploaded;
+        private RichTextBoxLog richTextBoxLog1;
+        private System.Windows.Forms.MaskedTextBox txt_timeRec;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label timmer;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+>>>>>>> Stashed changes
     }
 }
 
--- View/MainWindow.cs
+++ View/MainWindow.cs
@@ -12,8 +12,34 @@ namespace MESdbToERPdb
 {
     public partial class mes2ERPMainWin : Form
     {
+<<<<<<< Updated upstream
         protected int cycle;
         public MySqlConnection con = DatabaseUtils.GetMESDBConnection();
+=======
+        string Title = "";
+        EventBroker.EventObserver m_observerLog = null;
+
+        EventBroker.EventParam m_timerEvent = null;
+
+        FlowDocument m_flowDoc = null; //Hien log vao richtextbox
+        //Khoi tao bg worker
+        int timer = 0;
+
+        System.Threading.Timer tmrEnsureWorkerGetsCalled;
+        object lockObject = new object();
+        System.Windows.Forms.NotifyIcon m_notify = null; //icon trong bảng thông báo
+        SettingClass SettingClass = null; 
+        private void InitializeVersion()
+        {
+             
+            string version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.Major.ToString(); //AssemblyVersion을 가져온다.
+            version += "." + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.Minor.ToString();
+            version += "." + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.Build.ToString();
+            Title = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + " " + version;
+        }
+
+
+>>>>>>> Stashed changes
         public mes2ERPMainWin()
         {
             InitializeComponent();
@@ -65,6 +91,7 @@ namespace MESdbToERPdb
             textBox1.Text = string.Empty;
             textBox2.Text = string.Empty;
 
+<<<<<<< Updated upstream
             while (reader.Read())
             {
                 textBox1.Text += $"{reader.GetString("qc_no")}";
@@ -73,6 +100,56 @@ namespace MESdbToERPdb
             con.Close();
         }
 
+=======
+            //DataTable dt = new DataTable();
+            //mes.sqlDataAdapterFillDatatable(sqlSelectTop30, ref dt);
+
+            //BindingSource bSource = new BindingSource();
+            //bSource.DataSource = dt;
+            //dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            //dataGridView1.AllowUserToResizeRows = false;
+            //dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            //dataGridView1.AllowUserToResizeColumns = false;
+
+            //dataGridView1.DataSource = bSource;
+
+            //textBox1.Text = string.Empty;
+            //textBox2.Text = string.Empty;
+
+            ////while (reader.Read())
+            ////{
+            ////    textBox1.Text += $"{reader.GetString("qc_no")}";
+            ////    textBox2.Text += $"{reader.GetString("product_name")}";
+            ////}
+            ////con.Close();
+            ///
+
+            //tmrCallBgWorker.Start();
+            //btn_start.Text = "STARTING";
+            //btn_stop.Text = "STOP";
+            //btn_start.Enabled = false;
+            //btn_stop.Enabled = true;
+            //UploadMain uploadMain = new UploadMain();
+            //uploadMain.GetListLOT();
+            #endregion
+            
+            
+
+            tmrCallBgWorker.Start();
+            //UploadMain uploadMain = new UploadMain();
+            //uploadMain.GetListTransferOrder();
+            //btn_start.Enabled = false;
+            //btn_stop.Enabled = true;
+            ////insertERPSFCTC insert = new insertERPSFCTC();
+            ////insert.InsertdataToERP("B511-19120073;0010;B01;B01", "BMH1284056S02", "80", "0", "20211101", "00:00:00"); //test truyền dữ liệu tạo phiếu chuyển D201
+
+            ////rtb_log.Text = "Upload to data to ERP finished!";
+            //SystemLog.Output(SystemLog.MSG_TYPE.Nor, "Upload to data to ERP finished!", "");
+            //ClearMemory.CleanMemory();
+        }
+        #region backgroundworker
+       
+>>>>>>> Stashed changes
         private void btn_stop_Click(object sender, EventArgs e)
         {
             textBox1.Text = string.Empty;
@@ -100,6 +177,7 @@ namespace MESdbToERPdb
 
         private void timer_nextRun_Tick(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
             cycle--;
             if (0 == cycle)
             {
@@ -108,6 +186,49 @@ namespace MESdbToERPdb
                 cycle = (int)txt_cycle.Value * 3600;
             }
             txt_remainingSec.Text = cycle.ToString();
+=======
+            if (timer > 0)
+            {
+                timer--;
+                timmer.Text = timer.ToString();
+            }
+            else
+            {
+                if (Monitor.TryEnter(lockObject))
+                {
+                    try
+                    {
+                        // if bgworker is not busy then call the worker
+                        if (!bgWorker.IsBusy)
+                        {
+                            if (txt_timeRec.Text == "    -  -     :  :  .")
+                            {
+                                Properties.Settings.Default.TimeSet = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+                                Properties.Settings.Default.Save();
+                                txt_timeRec.Text = Properties.Settings.Default.TimeSet;
+                            }
+                            else
+                            {
+                                Properties.Settings.Default.TimeSet = txt_timeRec.Text;
+                                Properties.Settings.Default.Save();
+                            }                          
+                            bgWorker.RunWorkerAsync();
+                        }
+                            
+                    }
+                    finally
+                    {
+                        Monitor.Exit(lockObject);
+                    }
+                }
+                else
+                {
+                    // as the bgworker is busy we will start a timer that will try to call the bgworker again after some time
+                    tmrEnsureWorkerGetsCalled = new System.Threading.Timer(new TimerCallback(tmrEnsureWorkerGetsCalled_Callback), null, 0, 10);
+                }
+                timer = 3600;
+            }          
+>>>>>>> Stashed changes
         }
 
         private void btn_stopTimer_Click(object sender, EventArgs e)
@@ -128,7 +249,27 @@ namespace MESdbToERPdb
         }
 
         private void BW_DoWork(object sender, DoWorkEventArgs e)
+<<<<<<< Updated upstream
         {
+=======
+        {        
+            
+
+           // does a job like writing to serial communication, webservices etc
+           var worker = sender as BackgroundWorker;
+            try
+            {
+                UploadMain uploadMain = new UploadMain();
+                uploadMain.GetListTransferOrder();
+                SystemLog.Output(SystemLog.MSG_TYPE.Nor, "Upload to data to ERP finished!", "");
+                ClearMemory.CleanMemory();
+            }
+            catch (Exception ex)
+            {
+
+                SystemLog.Output(SystemLog.MSG_TYPE.Err, "Upload to data to ERP not successful!", ex.Message);
+            }
+>>>>>>> Stashed changes
 
         }
 
@@ -142,7 +283,79 @@ namespace MESdbToERPdb
 
         private void BW_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
+<<<<<<< Updated upstream
 
+=======
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                SystemLog.Output(SystemLog.MSG_TYPE.Err, "update UI error", ex.Message);
+            }
+        }
+
+        //private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        //{
+        //    //DataGridViewRow row = this.dataGridView1.CurrentRow;
+        //    //textBox1.Text = row.Cells["qc_no"].Value.ToString();
+        //    //textBox2.Text = row.Cells["finish_quantity"].Value.ToString();
+        //}
+        void tmrEnsureWorkerGetsCalled_Callback(object obj)
+        {
+            // this timer was started as the bgworker was busy before now it will try to call the bgworker again
+            if (Monitor.TryEnter(lockObject))
+            {
+                try
+                {
+                    if (!bgWorker.IsBusy)
+                        bgWorker.RunWorkerAsync();
+                }
+                finally
+                {
+                    Monitor.Exit(lockObject);
+                }
+                tmrEnsureWorkerGetsCalled = null;
+            }
+        }
+        #endregion
+
+        private void mes2ERPMainWin_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            tmrCallBgWorker.Tick -= new EventHandler(timer_nextRun_Tick);
+            bgWorker.DoWork -= new DoWorkEventHandler(BW_DoWork);
+            bgWorker.ProgressChanged -= BW_ProgressChanged;
+            bgWorker.RunWorkerCompleted -= new RunWorkerCompletedEventHandler(BW_RunWorkerCompleted);
+
+            if (m_timerEvent != null)
+                EventBroker.RemoveTimeEvent(EventBroker.EventID.etUpdateMe, m_timerEvent);
+            EventBroker.RemoveObserver(EventBroker.EventID.etLog, m_observerLog);
+            EventBroker.Relase();
+            Environment.Exit(0);
+        }
+
+        private void mes2ERPMainWin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.Visible = false;
+                this.notifyIcon1.Visible = true;
+            }
+        }
+
+        private void mes2ERPMainWin_Load(object sender, EventArgs e)
+        {
+            SettingClass = new SettingClass();
+            if (File.Exists(SaveObject.Pathsave))
+                SettingClass = (SettingClass)SaveObject.Load_data(SaveObject.Pathsave);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+>>>>>>> Stashed changes
         }
     }
 }
